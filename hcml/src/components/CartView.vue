88ad393309ee177fa7303bb73c6a5bbe9075b708
magical <sun920122@gmail.com>
2023-01-12 20:45:51 +0800

    Add Cart

---
+++ hcml/src/components/CartView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cart">
+    <div>123</div>
+    <ul>
+      <li v-for="product in products" :key="product.order">
+        <text>{{ product.count }}</text>
+        <text>\nHi\n</text>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartView",
+  props: {
+    msg: String,
+    products: [
+      {
+        order: Number,
+        name: String,
+        count: Number,
+        price: Number,
+        img: Image,
+      },
+    ],
+  },
+  emits: ["products"],
+};
+</script>
+
+<style scoped lang="scss">
+.cart {
+  padding-top: 7rem;
+  width: 100%;
+  min-height: 500px;
+  text-align: left;
+  background-color: #c291bb;
+}
+</style>
